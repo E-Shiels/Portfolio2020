0fb548dcce19cf0d0570eb51b2aa6fc31ec9a466
Eric Shiels <ericshiels2@gmail.com>
2020-03-07 19:24:29 -0500

    Add navbar JS

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
-});
+(function() {
+  var burger = document.querySelector('.burger');
+  var menu = document.querySelector('#' + burger.dataset.target);
+  burger.addEventListener('click', function() {
+    burger.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+  });
+})();
+
