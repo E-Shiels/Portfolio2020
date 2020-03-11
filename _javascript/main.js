6709b750101ada555b0487983e00b61c0c31e781
Eric Shiels <ericshiels2@gmail.com>
2020-03-10 21:10:24 -0400

    Add name generator fetching

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,9 +1,20 @@
-(function() {
+document.addEventListener('DOMContentLoaded', function() {
+  //Hamburger menu button
   var burger = document.querySelector('.burger');
   var menu = document.querySelector('#' + burger.dataset.target);
   burger.addEventListener('click', function() {
     burger.classList.toggle('is-active');
     menu.classList.toggle('is-active');
   });
-})();
 
+  fetch('https://uinames.com/api/')
+    .then(response => {
+      if (response.status !== 200) {
+        console.log(`Error: ${response.status}`);
+        return;
+      }
+      response.json().then((json) => {
+        console.log(json)
+      })
+    })
+});
