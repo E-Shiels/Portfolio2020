00cb7b561d059edc85be4d0e1f190567139cdff0
Eric Shiels <ericshiels2@gmail.com>
2020-03-22 19:58:31 -0400

    Making error message more descriptive

--- _javascript/main.js
+++ _javascript/main.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
               );
           });
       }
-      console.error(`Error: ${response.status}`);
+      console.error(`Error with fetching random placeholder name: ${response.status}`);
       return;
     }
     response.json().then(json => {
